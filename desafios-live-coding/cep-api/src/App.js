d0882842b96c5ede5406326956956a2be2a9a149
Leticia Campos <leticia.campos@totvs.com.br>
2020-03-16 23:29:36 -0300

    criacao do componente de busca cep

---
+++ desafios-live-coding/cep-api/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddressForm from './form/AddressForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AddressForm />
+    </div>
+  );
+}
+
+export default App;
