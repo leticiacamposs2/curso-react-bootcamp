c6f2cfe71ef494eb8e40a906e412db4c730feb6e
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-28 19:28:12 -0300

    usando o useEffects e useState para controle de estado e ciclo de vida

--- desafios/refatoracao-de-componente/me-transforme-em-funcao.js
+++ desafios/refatoracao-de-componente/me-transforme-em-funcao.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'semantic-react-ui'
 
-class ExampleComponent extends Component {
+function ExampleComponent(props) {
 
-    state = {
-        showForm: false,
-        email: "",
-        name: this.props.name
-    }
+    const [showForm, setShowForm] = useState(false)
+    const [email, setEmail] = useState("")
+    const [name, setName] = useState(props.name)
 
     componentDidMount() {
 
@@ -32,36 +30,32 @@ class ExampleComponent extends Component {
 
 
     toggleShowForm = (e) => {
-
         this.setState({
-            showForm: !this.state.showForm,
+            showForm: !setShowForm,
         })
     }
 
     // RENDERS ------------------------------
 
-    renderForm() {
+    const renderForm = () => {
         return <Form onSubmit={this.handleSubmit}>
-            <Form.Input name="name" value={this.state.name} onChange={this.handleFormChange} />
-            <Form.Input name="email" value={this.state.email} onChange={this.handleFormChange} />
+            <Form.Input name="name" value={name} onChange={e => setName(e.target.value)} />
+            <Form.Input name="email" value={email} onChange={e => setEmail(e.target.value)} />
             <Form.Button type="submit">Submit!</Form.Button>
         </Form>
     }
 
     // RENDER --------------------------------
-    render() {
-
         return (
             <div >
-                {this.state.showForm ? this.renderForm()
+                {showForm ? this.renderForm()
                     : <Fragment>
                         <h3>Hi, {this.state.name}</h3>
-                        <Button onClick={this.toggleShowForm} >Enter Email Address!</Button>
+                        <Button onClick={toggleShowForm} >Enter Email Address!</Button>
                     </Fragment>
                 }
             </div>
         )
-    }
 }
 
 export default ExampleComponent
