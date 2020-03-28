02efc0dbf2f816c13363181da10b89d2a3d6929a
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-28 14:26:01 -0300

    criacao do formInput

--- desafios/form-cadastro-alunas/src/components/FormInput/index.jsx
+++ desafios/form-cadastro-alunas/src/components/FormInput/index.jsx
@@ -1,18 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const FormInput = () => ({ label, value, name, inputType, placeholder}) => {
+class FormInput extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.stateInicial = {
+            name: "",
+            city: "",
+            email: "",
+            cpf: "",
+            phone: ""
+        }
+    
+        this.state = this.stateInicial;
+    }
 
-    return (
-        <div>
-            <label>{label}</label>
-            <input 
-                value={value}
-                name={name}
-                type={inputType}
-                placeholder={placeholder}
-            />
-        </div>
-    );
+    escutadorDeInput = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    render() {
+
+        const { name, city, email, cpf, phone } = this.state;
+
+        return (
+            <form>
+                <label>nome completo</label><br/>
+                <input
+                    type="text"
+                    name="nome"
+                    value={name}
+                    onChange={this.escutadorDeInput}/>
+                <p/>
+
+                <label>cidade</label><br/>
+                <input 
+                    type="text"
+                    name="cidade"
+                    value={city}
+                    onChange={this.escutadorDeInput}/>
+                <p/>
+
+                <label>email</label><br/>
+                <input 
+                    type="text"
+                    name="email"
+                    value={email}
+                    placeholder="email@email.com"
+                    onChange={this.escutadorDeInput}/>
+                <p/>
+
+                <label>cpf</label><br/>
+                <input
+                    type="text"
+                    name="cpf"
+                    value={cpf}
+                    placeholder="000.000.000-00"
+                    onChange={this.escutadorDeInput} />
+                <p/>
+
+                <label>telefone</label><br/>
+                <input
+                    type="text"
+                    name="phone"
+                    value={phone}
+                    placeholder="(xx) xxxxx-xxxx"
+                    onChange={this.escutadorDeInput} />
+                <p/>
+            </form>
+        );
+    }
 }
 
 export default FormInput;
--- desafios/form-cadastro-alunas/src/components/RegisterForm/index.jsx
+++ desafios/form-cadastro-alunas/src/components/RegisterForm/index.jsx
@@ -1,62 +1,16 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
 import FormInput from '../FormInput';
 
-class RegisterForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            city: "",
-            email: "",
-            cpf: "",
-            phone: ""
-        };
-    }
+class RegisterForm extends Component {
 
     render() {
         return (
-            <div>
-                <p>aaaaaaaa</p>
-                <form>
-                    <FormInput 
-                        label="nome completo"
-                        value={this.state.name}
-                        name="name"
-                    />
-
-                    <FormInput 
-                        label="cidade"
-                        value={this.state.city}
-                        name="city"
-                    />
-
-                    <FormInput 
-                        inputType="email"
-                        label="email"
-                        placeholder="email@email.com"
-                        value={this.state.email}
-                        name="email"
-                    />
-
-                    <FormInput
-                        label="cpf"
-                        placeholder="000.000.000-00"
-                        value={this.state.cpf}
-                        name="cpf"
-                    />
-
-                    <FormInput 
-                        label="telefone"
-                        placeholder="(xx) xxxxx-xxxx"
-                        value={this.state.phone}
-                        name="phone"
-                    />
-                </form>
-            </div>
+            <Fragment>
+                <FormInput/>
+            </Fragment>
         );
     }
-
 }
 
 export default RegisterForm;
