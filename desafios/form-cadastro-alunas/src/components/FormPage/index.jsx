c289de972577b9cad7e8e796deb6e0d26128d91e
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-27 22:47:23 -0300

    criacao do componente pai

---
+++ desafios/form-cadastro-alunas/src/components/FormPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const FormPage = () => (
+    <div className="container">
+        <h3>Cadastro de alunas</h3>
+    </div>
+);
+
+export default FormPage;
