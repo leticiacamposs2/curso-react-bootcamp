bb8d29cadffab1b0440f3dc493f7f070122e8787
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-29 01:02:32 -0300

    criando o reducers visibilityFilter

--- desafios/todo-list-challenge/src/reducers/visibilityFilter.js
+++ desafios/todo-list-challenge/src/reducers/visibilityFilter.js
@@ -1,10 +1,13 @@
 //reducer para setar o tipo de todo listado no app
 //pense sempre o que está setado como default na store (state) e que você deve alterar esse estado
 
-
-const visibilityFilter = (state, action) => {
-    
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter
+        default:
+            return state
+    }
 }
-
 
 export default visibilityFilter
