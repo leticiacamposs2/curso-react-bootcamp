f574e28a528717f4dd792b9e45fc53b1795f4eef
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-29 13:49:50 -0300

    criando a pagina sobre

--- desafios/react-router/react-com-rotas/src/App.js
+++ desafios/react-router/react-com-rotas/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1 className="App-title">Welcome to React</h1>
+       </header >
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+       </p>
+    </div >
+   );
+  }
 }
-
 export default App;
