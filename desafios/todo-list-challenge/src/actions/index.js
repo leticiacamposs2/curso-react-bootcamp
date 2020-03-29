98d01a3eafa0447760309f75a91562c4b4c33f6a
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-29 00:59:54 -0300

    criacao das actions

--- desafios/todo-list-challenge/src/actions/index.js
+++ desafios/todo-list-challenge/src/actions/index.js
@@ -1,14 +1,26 @@
-export const addTodo = text => ({
-  
-})
+let nextTodoId = 0
 
-export const setVisibilityFilter = filter => ({
-  
-})
+export const addTodo = text => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  }
+}
 
-export const toggleTodo = id => ({
-  
-})
+export const setVisibilityFilter = filter => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+}
+
+export const toggleTodo = id => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
 
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
