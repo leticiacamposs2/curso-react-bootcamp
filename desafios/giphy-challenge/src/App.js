79612b32b3936be027225873d61e39a413e6318a
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-03-31 00:54:29 -0300

    consumindo a api do giphy e criando uma pagina simples

--- desafios/giphy-challenge/src/App.js
+++ desafios/giphy-challenge/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Giphy from './Giphy';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      clicked: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      clicked: true
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Pesquise um gif: </label>
+        <input />
+        <button onClick={this.handleClick}>Pesquisar</button>
+        {this.state.clicked ? <Giphy /> : null}
+      </div>
+    );
+  }
+};
 
 export default App;
---
+++ desafios/giphy-challenge/src/Giphy.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { Gif } from "@giphy/react-components";
+
+function Giphy(props) {
+    const [gifs, setGifs] = useState(null);
+    const key = 'cUdC5Slzcn5tlczQGYIuGpBPLWGEdAc5'
+    const query = 'dogs'
+    const limit = 5
+
+    useEffect(() => {
+        fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${key}&limit=${limit}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setGifs(result)
+                },
+                (error) => {
+                    console.log('error', error);
+                }
+            )
+    }, [query])
+
+    return (
+        <div>
+            { gifs
+                && gifs.data.map(gif => {
+                    return <Gif key={gif.id} gif={gif} width={200} />
+                })
+            }
+        </div>
+    )
+
+}
+
+export default Giphy;
