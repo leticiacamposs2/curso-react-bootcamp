0de39173746028c8c32351b45fd31b77d70c8112
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-04-04 10:42:05 -0300

    instalando o jest no projeto e criando meu primeiro teste

--- aula-8/usando-jest/src/App.test.js
+++ aula-8/usando-jest/src/App.test.js
@@ -1,9 +1,31 @@
+// import { render } from '@testing-library/react';
+// import { shallow } from 'enzyme';
+
+// it('renders without crashing', () => {
+//   shallow(<App />);
+// });
+
+// it('renders welcome message', () => {
+//   const wrapper = shallow(<App />);
+//   const welcome = <h2>Welcome to React</h2>;
+//   // expect(wrapper.contains(welcome)).toBe(true);
+//   expect(wrapper.contains(welcome)).toEqual(true);
+// });
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    mount(<App />);
+  });
+})
---
+++ aula-8/usando-jest/src/components/Movies/Movies.js
@@ -0,0 +1,7 @@
+const video = {
+    play() {
+        return true;
+    },
+};
+
+export default video;
